205acbe034d9f584df135f93b72eb9ad7829036a
AndrewBurUU <andriv80@gmail.com>
2023-04-03 19:39:08 +0800

    3. @ModelAttribute. Создание инцидента. [#261013]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.service.*;
+import ru.job4j.accidents.model.Accident;
 
 @Controller
 @RequestMapping("/accidents")
@@ -15,6 +16,18 @@ public class AccidentController {
 
     @GetMapping("/all")
     public String getAll(Model model) {
+        model.addAttribute("accidents", accidentService.findAll());
+        return "accidents/all";
+    }
+
+    @GetMapping("/createAccident")
+    public String viewCreateAccident() {
+        return "accidents/createAccident";
+    }
+
+    @PostMapping("/saveAccident")
+    public String save(@ModelAttribute Accident accident, Model model) {
+        accidentService.save(accident);
         model.addAttribute("accidents", accidentService.findAll());
         return "accidents/all";
     }
