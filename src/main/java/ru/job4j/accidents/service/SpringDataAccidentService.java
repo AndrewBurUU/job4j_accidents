204579b8b01dbc9d6899e59f675a531c95e593c1
AndrewBurUU <andriv80@gmail.com>
2023-04-06 22:32:05 +0800

    2. Spring Data [#296073 #407554]

--- src/main/java/ru/job4j/accidents/service/SpringDataAccidentService.java
+++ src/main/java/ru/job4j/accidents/service/SpringDataAccidentService.java
@@ -2,22 +2,30 @@ package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.*;
 import ru.job4j.accidents.repository.AccidentRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class SpringDataAccidentService {
+
     private final AccidentRepository accidentRepository;
 
     public void create(Accident accident) {
         accidentRepository.save(accident);
     }
 
-    public List<Accident> getAll() {
-        return (List<Accident>) accidentRepository.findAll();
+    public void update(Accident accident) {
+        accidentRepository.save(accident);
+    }
+
+    public List<Accident> findAll() {
+        return accidentRepository.findAll();
+    }
+
+    public Optional<Accident> findById(int id) {
+        return accidentRepository.findById(id);
     }
 }
