17def1796f95b1828b5158b1abe6afa1c904e2b2
AndrewBurUU <andriv80@gmail.com>
2023-04-10 11:13:32 +0800

    0. Spring Security [#6879 #408386]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,15 +1,16 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.security.core.context.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accidents.repository.*;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/", "/index"})
-    public String getIndex(Model model) {
-        model.addAttribute("user", "Petr Arsentev");
+    public String index(Model model) {
         return "index";
     }
 }
--- src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
@@ -1,6 +1,5 @@
 package ru.job4j.accidents.repository;
 
-import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accidents.model.Accident;
 
@@ -8,7 +7,6 @@ import java.util.*;
 
 public interface AccidentRepository extends CrudRepository<Accident, Integer> {
 
-    @Query("from Model")
     List<Accident> findAll();
 
 }
